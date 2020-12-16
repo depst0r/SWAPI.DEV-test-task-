704c9b43d55440f7b44c8a8e03493365f878d643
depst0r <depstor90@gmail.com>
2020-12-16 17:12:35 +0300

    'form-inputs'

--- src/Components/FormReview/FormReview.jsx
+++ src/Components/FormReview/FormReview.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const FormReviev = () => {
+
+    const [userName, setUserName] = useState('')
+    const [email, setEmail] = useState('')
+    const [review, setReview] = useState('')
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -10,17 +14,39 @@ export const FormReviev = () => {
 <form onSubmit={handleSubmit}>
 <div className="form-group">
         <label htmlFor="exampleInputUserName">User Name</label>
-        <input type="text" className="form-control" id="exampleInputUserName" placeholder="User Name"/>
+        <input 
+        type="text" 
+        className="form-control" 
+        id="exampleInputUserName" 
+        placeholder="User Name"
+        value={userName}
+        onChange={e => setUserName(e.target.value)}
+        />
     </div>
     <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        <input 
+        type="email" 
+        className="form-control" 
+        id="exampleInputEmail1" 
+        aria-describedby="emailHelp" 
+        placeholder="Enter email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        />
     </div>
     <div className="form-group">
         <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea 
+        className="form-control" 
+        id="exampleFormControlTextarea1" 
+        rows="3"
+        value={review}
+        onChange={e => setReview(e.target.value)}
+        />
     </div>
-    <button  className="btn btn-primary">Submit</button>
+    <button 
+    className="btn btn-primary">Submit</button>
 </form>
     </>
 }
