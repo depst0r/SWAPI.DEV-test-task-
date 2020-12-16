02caf9aaa50b503d5bc66f9569c8c6dccdfc69be
depst0r <depstor90@gmail.com>
2020-12-16 14:51:17 +0300

    'id'

--- src/Components/FilmsDiteils/FilmsDiteils.jsx
+++ src/Components/FilmsDiteils/FilmsDiteils.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState ,useEffect } from 'react'
 import './FilmsDiteils.css'
 
-export const FilmsDiteils = () => {
+export const FilmsDiteils = ({filmIndex}) => {
     return<>
     <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap"/>
+        <img className="card-img-top" src="https://media.comicbook.com/2020/07/star-wars-skywalker-saga-wallpaper-1231363.jpeg?auto=webp&width=1280&height=720&crop=1280:720,smart" alt="films"/>
     <div className="card-body">
         <ul className="list-group list-group-flush">
             <li className="list-group-item">Cras justo odio</li>
