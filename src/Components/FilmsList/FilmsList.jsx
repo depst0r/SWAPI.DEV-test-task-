3b1679ace274766a5947add521ac9c8f6ba8145d
depst0r <depstor90@gmail.com>
2020-12-16 13:48:30 +0300

    'fl'

--- src/Components/FilmsList/FilmsList.jsx
+++ src/Components/FilmsList/FilmsList.jsx
@@ -1,9 +1,10 @@
 import React, {useState ,useEffect } from 'react'
 import './FilmsList.css'
+import { Spinner } from '../Spinner/Spinner'
 
 export const FilmsList = () => {
 
-    const [films, setFilms] = useState([])
+    const [films, setFilms] = useState('')
 
     useEffect(() => {
        fetch('https://swapi.dev/api/films/')
@@ -13,15 +14,17 @@ export const FilmsList = () => {
 
     return<>
         <div>
-            {films.map((res, index) => {
+            {films ? films.map((film, index) => {
                 return (
                     <ul className="item-list list-group" key={index}>
-                        <li className="list-group-item">
-                            {res.title}
+                        <li 
+                        className="list-group-item"
+                        >
+                            {film.title}
                         </li>
                     </ul>
                 )
-            })}
+            }): <Spinner/> }
         </div>
     </>
 }
