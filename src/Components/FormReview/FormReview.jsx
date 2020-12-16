7ddd711518b679ce036fa1ec1438a44bbc809afc
depst0r <depstor90@gmail.com>
2020-12-16 16:10:24 +0300

    'form-start'

--- src/Components/FormReview/FormReview.jsx
+++ src/Components/FormReview/FormReview.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
 export const FormReviev = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault()
+    } 
+
     return<>
-        <form>
-  <div className="form-group">
-    <label htmlFor="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-  </div>
-  <div className="form-group">
-    <label htmlFor="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div className="form-group">
-    <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <button  className="btn btn-primary">Submit</button>
+<form onSubmit={handleSubmit}>
+<div className="form-group">
+        <label htmlFor="exampleInputUserName">User Name</label>
+        <input type="text" className="form-control" id="exampleInputUserName" placeholder="User Name"/>
+    </div>
+    <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    </div>
+    <div className="form-group">
+        <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <button  className="btn btn-primary">Submit</button>
 </form>
     </>
 }
